0a91b0c21db03f5975c4e67d9ec64b5bc3a3b7ef
muricicco <murian.caetano@gmail.com>
2018-03-01 14:53:43 -0300

    3modificacao com 3 hello world

--- arquivo.py
+++ arquivo.py
@@ -1,2 +1,10 @@
+#-*-coding :utf-8 -*-
 print "Hello"
 print "mod "
+print " 3 modificacao"
+print " Branch Funcionalidade"
+print "Insira um numero"
+a=3
+b=9
+c=a+b
+print c
---
+++ minhapagina.js
@@ -0,0 +1,8 @@
+  // document.write("Hello World 1!");
+// setview é a centragem do mapa precisa de coodenadas sempre que nao for declarado é wgs84
+// o 11 equivale ao zoom. 1= mundo e 18 menor escala 1/2000.
+window.onload= function (){
+  var mapa = L.map('meumapa').setView([-25.45,-49.27],13);
+//Mapbox
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mapa);
+}
